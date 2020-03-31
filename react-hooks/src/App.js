572f396066c8d75a268b4f00977350389e6a36ab
@telecristy <telematicacristina@gmail.com>
2020-03-30 18:06:52 -0700

    Adding react hook explanation

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+// Dependencies
+import React, { useState, useEffect } from 'react';
+
+// Styles
 import './App.css';
 
 function App() {
+  // Local state
+  const [count, setCount] = useState(0);
+
+  // useEffect runs both after the first render and after every update
+  useEffect(() => { // This arrow function as our "effect"
+
+    // useEffect it will remember the effect we used
+    // useEffect runs after every update
+    console.log('Clicked times ->', count); // each effect “belongs” to a particular render
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Clicked times { count }</p>
+
+      <button
+        type="button"
+        onClick={() => setCount(count + 1)} // Every time we re-render, we schedule a different effect, replacing the previous one
+      >
+        Click me!
+      </button>
     </div>
   );
 }
